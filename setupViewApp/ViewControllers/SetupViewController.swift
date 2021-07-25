2d299c31dca21e03f7df4785182fe686ebff2463
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-07-26 02:40:39 +0300

    mainViewController to setupViewController and back functional v 2.0

--- setupViewApp/ViewControllers/SetupViewController.swift
+++ setupViewApp/ViewControllers/SetupViewController.swift
@@ -88,7 +88,6 @@ class SetupViewController: UIViewController {
     private func setBackGroundColor() {
         mainView.backgroundColor = mainScreenBackGroundColor
         
-//        mainView.backgroundColor = UIColor (red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
     
     private func setTextFied() {
