45ffa1aa156c33da122a9ffb0416dfd2dfdbb4d4
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-07-26 02:05:05 +0300

    mainViewController to setupViewController and back functional v 2.0

--- setupViewApp/ViewControllers/MainViewController.swift
+++ setupViewApp/ViewControllers/MainViewController.swift
@@ -7,18 +7,35 @@
 
 import UIKit
 
+
+protocol SetupViewControllerDelegate {
+    func setColors(red: CGFloat, green: CGFloat, blue: CGFloat)
+    func updateMainScreenBackGroundColor(red: CGFloat, green: CGFloat, blue: CGFloat)
+}
+
 class MainViewController: UIViewController {
     
-    var mainScreenBackGroundColor: UIColor = UIColor(red: 1, green: 1, blue: 0, alpha: 1)
+    var mainScreenBackGroundColor: UIColor = UIColor (red: 1, green: 1, blue: 1, alpha: 1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
-    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let mainScreenVC = segue.destination as? SetupViewController else { return}
         mainScreenVC.mainScreenBackGroundColor = mainScreenBackGroundColor
+        mainScreenVC.delegate = self
+    }
+}
+
+
+extension MainViewController: SetupViewControllerDelegate {
+    func updateMainScreenBackGroundColor(red: CGFloat, green: CGFloat, blue: CGFloat) {
+        mainScreenBackGroundColor = UIColor (red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+    func setColors(red: CGFloat, green: CGFloat, blue: CGFloat) {
+        view.backgroundColor = UIColor (red: red, green: green, blue: blue, alpha: 1)
     }
 }
--- setupViewApp/ViewControllers/SetupViewController.swift
+++ setupViewApp/ViewControllers/SetupViewController.swift
@@ -7,9 +7,8 @@
 
 import UIKit
 
+
 class SetupViewController: UIViewController {
-    
-    var mainScreenBackGroundColor: UIColor?
         
     @IBOutlet weak var mainView: UIView!
     
@@ -25,13 +24,15 @@ class SetupViewController: UIViewController {
     @IBOutlet weak var greenTextField: UITextField!
     @IBOutlet weak var blueTextField: UITextField!
     
+    var mainScreenBackGroundColor: UIColor?
+    var delegate: SetupViewControllerDelegate!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         mainView.layer.cornerRadius = 15
-//        mainView.backgroundColor = mainScreenBackGroundColor
+        
         
         redSlider.maximumTrackTintColor = #colorLiteral(red: 0.9999018312, green: 1, blue: 0.9998798966, alpha: 1)
         redSlider.minimumTrackTintColor = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
@@ -42,36 +43,60 @@ class SetupViewController: UIViewController {
         blueSlider.maximumTrackTintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         blueSlider.minimumTrackTintColor = #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1)
         
-        redLabel.text = String(format: "%.2f", redSlider.value)
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
-        
-        redTextField.text = redLabel.text
-        greenTextField.text = greenLabel.text
-        blueTextField.text = blueLabel.text
-        
+        updateSliderToLabel()
+        setTextFied()
         setColor()
-        mainView.backgroundColor = mainScreenBackGroundColor
-
+        setBackGroundColor()
     }
     
+
     
     @IBAction func rgbChanges(_ sender: UISlider) {
         setColor()
-        
-        redLabel.text = String(format: "%.2f", redSlider.value)
-        redTextField.text = redLabel.text
-        greenLabel.text = String(format: "%.2f", greenSlider.value)
-        greenTextField.text = greenLabel.text
-        blueLabel.text = String(format: "%.2f", blueSlider.value)
-        blueTextField.text = blueLabel.text
+        setTextFied()
+        updateSliderToLabel()
     }
     
     
-   
+    @IBAction func doneButton() {
+        guard let doubleRedLabel = Double(redLabel.text ?? "0") else {return}
+        guard let doubleGreenLabel = Double(greenLabel.text ?? "0") else {return}
+        guard let doubleBlueLabel = Double(blueLabel.text ?? "0") else {return}
+        
+        delegate.setColors(
+            red: CGFloat(doubleRedLabel),
+            green: CGFloat(doubleGreenLabel),
+            blue: CGFloat(doubleBlueLabel)
+        )
+        delegate.updateMainScreenBackGroundColor(
+            red: CGFloat(doubleRedLabel),
+            green: CGFloat(doubleGreenLabel),
+            blue: CGFloat(doubleBlueLabel)
+        )
+        dismiss(animated: true)
+    }
     
     
     // MARK: -private function
+    
+    private func updateSliderToLabel() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    private func setBackGroundColor() {
+        mainView.backgroundColor = mainScreenBackGroundColor
+        
+//        mainView.backgroundColor = UIColor (red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    private func setTextFied() {
+        redTextField.text = redLabel.text
+        greenTextField.text = greenLabel.text
+        blueTextField.text = blueLabel.text
+    }
+    
     private func setColor() {
         mainView.backgroundColor = UIColor(
             red: CGFloat(redSlider.value),
